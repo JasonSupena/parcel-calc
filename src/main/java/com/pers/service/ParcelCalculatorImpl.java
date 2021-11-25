4d52e3187bb103f7ea16bf425a76711beb1aad63
Jason Supena <supenajason@gmail.com>
2021-11-25 14:22:12 +0800

    adding other logic

--- src/main/java/com/pers/service/ParcelCalculatorImpl.java
+++ src/main/java/com/pers/service/ParcelCalculatorImpl.java
@@ -1,5 +1,7 @@
 package com.pers.service;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Service;
 
 import com.pers.model.MonetaryAmount;
@@ -11,10 +13,36 @@ public class ParcelCalculatorImpl implements ParcelCalculatorApi {
 
 	@Override
 	public MonetaryAmount calculate(ParcelRequest request) throws ParcelRejectedException {
-		if(request.getWeight() > 50) {
+		MonetaryAmount parcelAmount = new MonetaryAmount();
+		int volume = request.getHeight() * request.getWidth() * request.getLength();
+		int parcelWeight = request.getWeight();
+		
+		if(parcelWeight > 50) {
 			throw new ParcelRejectedException("Parcel weight is too heavy");
 		}
-		return new MonetaryAmount();
+		
+		if(parcelWeight > 10 && parcelWeight <= 50) {
+			parcelAmount.setAmount(new BigDecimal(20 * parcelWeight));
+			parcelAmount.setCurrencyCode("PHP");
+			return parcelAmount;
+		}
+		
+		if(volume < 1500) {
+			parcelAmount.setAmount(new BigDecimal(.03 * volume));
+			parcelAmount.setCurrencyCode("PHP");
+			return parcelAmount;
+		}
+		
+		if(volume > 1500 && volume < 2500) {
+			parcelAmount.setAmount(new BigDecimal(.04 * volume));
+			parcelAmount.setCurrencyCode("PHP");
+			return parcelAmount;
+		}
+		
+		parcelAmount.setAmount(new BigDecimal(.05 * volume));
+		parcelAmount.setCurrencyCode("PHP");
+		
+		return parcelAmount;
 	}
 
 }
--- src/test/java/com/pers/service/ParcelCalculatorImplTest.java
+++ src/test/java/com/pers/service/ParcelCalculatorImplTest.java
@@ -1,11 +1,14 @@
 package com.pers.service;
 
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.pers.model.MonetaryAmount;
 import com.pers.model.ParcelRejectedException;
 import com.pers.model.ParcelRequest;
 
@@ -33,5 +36,47 @@ public class ParcelCalculatorImplTest {
 			request.setWeight(51);
 			calc.calculate(request);
 		});
+	}
+	
+	@Test
+	public void shouldCalculateHeavyParcel() throws Throwable {
+		ParcelRequest request = new ParcelRequest();
+		request.setWeight(11);
+		MonetaryAmount amount = calc.calculate(request);
+		Assertions.assertEquals("PHP", amount.getCurrencyCode());
+		Assertions.assertEquals(new BigDecimal(220), amount.getAmount());
+	}
+	
+	@Test
+	public void shouldCalculateSmallParcel() throws Throwable {
+		ParcelRequest request = new ParcelRequest();
+		request.setHeight(10);
+		request.setWidth(10);
+		request.setLength(10);
+		MonetaryAmount amount = calc.calculate(request);
+		Assertions.assertEquals("PHP", amount.getCurrencyCode());
+		Assertions.assertEquals(new BigDecimal(30), amount.getAmount());
+	}
+	
+	@Test
+	public void shouldCalculateMediumParcel() throws Throwable {
+		ParcelRequest request = new ParcelRequest();
+		request.setHeight(10);
+		request.setWidth(10);
+		request.setLength(20);
+		MonetaryAmount amount = calc.calculate(request);
+		Assertions.assertEquals("PHP", amount.getCurrencyCode());
+		Assertions.assertEquals(new BigDecimal(80), amount.getAmount());
+	}
+	
+	@Test
+	public void shouldCalculateLargeParcel() throws Throwable {
+		ParcelRequest request = new ParcelRequest();
+		request.setHeight(10);
+		request.setWidth(20);
+		request.setLength(30);
+		MonetaryAmount amount = calc.calculate(request);
+		Assertions.assertEquals("PHP", amount.getCurrencyCode());
+		Assertions.assertEquals(new BigDecimal(300), amount.getAmount());
 	}
 }
